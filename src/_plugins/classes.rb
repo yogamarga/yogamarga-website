c17b708b4e6a4ca96abfe8c72b780956a85a267b
Christos Tsakostas <c.tsakostas@gmail.com>
2014-05-13 09:32:09 +0300

    Arketa

--- src/_plugins/classes.rb
+++ src/_plugins/classes.rb
@@ -15,19 +15,19 @@ module Jekyll
         end
 
         def get_all_classes(site)
-            # puts "1 - AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+            puts "1 - AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
             {}.tap do |classes|
-                # puts "2 - " + Dir.pwd
+                puts "2 - " + Dir.pwd
                 Dir['_classes/*.yml'].each do |path|
-                    # puts "3 - AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+                    puts "3 - AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
                     name   = File.basename(path, '.yml')
                     config = YAML.load(File.read(File.join(@base, path)))
                     type   = config['type']
                     category = config['category']
 
-                    # puts "type " + type
-                    # puts "name " + name
-                    # puts "category " + category
+                    puts "type " + type
+                    puts "name " + name
+                    puts "category " + category
 
                     if config['active']
                         classes[category] = {} if classes[category].nil?
@@ -40,6 +40,20 @@ module Jekyll
     end # AllClassesIndex
 
 
+    # Generates a page for the specified class
+    class ClassIndex < Page
+        def initialize(site, base, dir, path, lang)
+            @site = site
+            @base = base
+            @dir  = dir
+            @name = "index.html"
+
+            self.read_yaml(File.join(base, '_layouts'), 'class.html')
+            self.data = YAML.load(File.read(File.join(@base + "/_classes", path + ".yml")))
+            self.data['lang']= lang
+            self.process(@name)
+        end
+    end
 
 
     # GENERATORS
@@ -49,21 +63,22 @@ module Jekyll
             priority :normal
 
             def generate(site)
-                # puts "11111111111111111111111111111111111111111111111111111111111111111111111111111111"
+                puts "11111111111111111111111111111111111111111111111111111111111111111111111111111111"
                 generate_all_classes(site)
             end
 
             # Loops through the list of classes and processes each one of them.
             def generate_all_classes(site)
-                # puts "222222222222222222222222222222222222222222222222222222222222"
+                puts "222222222222222222222222222222222222222222222222222222222222"
                 if Dir.exists?('src/_classes')
-                    # puts "3333333333"
+                    puts "3333333333"
                     Dir.chdir('src/_classes')
 
                     Dir["*.yml"].each do |path|
-                        # puts "444444444 " + path
+                        puts "444444444 " + path
                         name = File.basename(path, '.yml')
-                        # self.generate_class_index(site, "_team/#{path}", name)
+                        self.generate_class_index(site, site.source, "/pages/classes/" + name, name, 'el')
+                        self.generate_class_index(site, site.source, "/en/pages/classes/" + name, name, 'en')
                     end
 
                     Dir.chdir(site.source)
@@ -73,7 +88,7 @@ module Jekyll
 
             # Generates the index page for all of the classes
             def generate_all_classes_index(site)
-                # puts "55555555555555555555555555555555555555555555555555555555"
+                puts "55555555555555555555555555555555555555555555555555555555"
                 allClasses = AllClassesIndex.new(site, site.source, "/pages/classes")
                 allClasses.render(site.layouts, site.site_payload)
                 allClasses.write(site.dest)
@@ -83,8 +98,19 @@ module Jekyll
             end
 
             # Generates the page for a specific class
-            def generate_class_index(site)
-                # puts "666666666666666666666666666666666666666666666666"
+            def generate_class_index(site, base, dir, path, lang)
+                # puts "666666666666666666666666666666666666666666666666 base " + base
+                # puts "666666666666666666666666666666666666666666666666 dir " + dir
+                # puts "666666666666666666666666666666666666666666666666 path " + path
+                cLaSs = ClassIndex.new(site, base, dir, path, lang)
+                if cLaSs.data['active']
+                    # puts "77777777777777777777777777777777777777777777777777777777"
+                    cLaSs.render(site.layouts, site.site_payload)
+                    cLaSs.write(site.dest)
+
+                    site.pages << cLaSs
+                    site.static_files << cLaSs
+                end
 
             end
 
--- src/_plugins/teachers.rb
+++ src/_plugins/teachers.rb
@@ -15,19 +15,19 @@ module Jekyll
         end
 
         def get_all_teachers(site)
-            puts "1 - AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+            # puts "1 - AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
             {}.tap do |teachers|
-                puts "2 - " + Dir.pwd
+                # puts "2 - " + Dir.pwd
                 Dir['_teachers/*.yml'].each do |path|
-                    puts "3 - AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+                    # puts "3 - AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
                     name   = File.basename(path, '.yml')
                     config = YAML.load(File.read(File.join(@base, path)))
                     type   = config['type']
                     category = config['category']
 
-                    puts "type " + type
-                    puts "name " + name
-                    puts "category " + category
+                    # puts "type " + type
+                    # puts "name " + name
+                    # puts "category " + category
 
                     if config['active']
                         teachers[type] = {} if teachers[type].nil?
@@ -49,11 +49,11 @@ module Jekyll
 
             self.read_yaml(File.join(base, '_layouts'), 'teacher.html')
             self.data = YAML.load(File.read(File.join(@base + "/_teachers", path + ".yml")))
+            self.data['title'] = "#{self.data['name']}"
+            self.data['breadcrumb'] = "#{self.data['name']}"
             self.process(@name)
         end
     end
-
-
 
     # GENERATORS
     module Generators
@@ -62,19 +62,19 @@ module Jekyll
             priority :normal
 
             def generate(site)
-                puts "11111111111111111111111111111111111111111111111111111111111111111111111111111111"
+                # puts "11111111111111111111111111111111111111111111111111111111111111111111111111111111"
                 generate_all_teachers(site)
             end
 
             # Loops through the list of teachers and processes each one of them.
             def generate_all_teachers(site)
-                puts "222222222222222222222222222222222222222222222222222222222222 " + Dir.pwd
+                # puts "222222222222222222222222222222222222222222222222222222222222 " + Dir.pwd
                 if Dir.exists?('_teachers')
-                    puts "3333333333"
+                    # puts "3333333333"
                     Dir.chdir('_teachers')
 
                     Dir["*.yml"].each do |path|
-                        puts "444444444 " + path
+                        # puts "444444444 " + path
                         name = File.basename(path, '.yml')
                         self.generate_teacher_index(site, site.source, "/pages/teachers/" + name, name)
                     end
@@ -86,7 +86,7 @@ module Jekyll
 
             # Generates the index page for all of the teachers
             def generate_all_teachers_index(site)
-                puts "55555555555555555555555555555555555555555555555555555555"
+                # puts "55555555555555555555555555555555555555555555555555555555"
                 allTeachers = AllTeachersIndex.new(site, site.source, "/pages/teachers")
                 allTeachers.render(site.layouts, site.site_payload)
                 allTeachers.write(site.dest)
@@ -98,12 +98,12 @@ module Jekyll
 
             # Generates the page for a specific teacher
             def generate_teacher_index(site, base, dir, path)
-                puts "666666666666666666666666666666666666666666666666 base " + base
-                puts "666666666666666666666666666666666666666666666666 dir " + dir
-                puts "666666666666666666666666666666666666666666666666 path " + path
+                # puts "666666666666666666666666666666666666666666666666 base " + base
+                # puts "666666666666666666666666666666666666666666666666 dir " + dir
+                # puts "666666666666666666666666666666666666666666666666 path " + path
                 teacher = TeacherIndex.new(site, base, dir, path)
                 if teacher.data['active']
-                    puts "77777777777777777777777777777777777777777777777777777777"
+                    # puts "77777777777777777777777777777777777777777777777777777777"
                     teacher.render(site.layouts, site.site_payload)
                     teacher.write(site.dest)
 
